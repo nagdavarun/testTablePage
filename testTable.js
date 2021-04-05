fa63fc0fc8cfe20e609cbf30d932987055cafd38
nagdavarun <varun.nagda@exponentia.ai>
2021-04-05 20:56:08 +0530

    comments added and minor issue solved

--- testTable.js
+++ testTable.js
@@ -9,6 +9,17 @@ function jsonData() {
             airportJson = data
         });
 }
+
+// call table and JSON data
+jsonData()
+createTable()
+
+//remove loader
+setTimeout(() => {
+    document.getElementById("spinner").remove()
+}, 2500);
+
+// mobile responsive css
 setTimeout(() => {
     if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         document.getElementById("filter_search").style.display = "block";
@@ -19,7 +30,8 @@ setTimeout(() => {
         document.getElementById("searchHeader").style.paddingBottom = "10px";
     }
 }, 100);
-// search table
+
+// search table filter
 (function (document) {
     'use strict';
 
@@ -62,9 +74,9 @@ setTimeout(() => {
 
 
 // filter for checkbox
+
 "use strict";
 Array.prototype.flexFilter = function (info) {
-
     var matchesFilter, matches = [], count;
     matchesFilter = function (item) {
         count = 0
@@ -75,39 +87,35 @@ Array.prototype.flexFilter = function (info) {
         }
         return count == info.length;
     }
-
+    // Loop through each item in the array
     for (var i = 0; i < this.length; i++) {
         if (matchesFilter(this[i])) {
             matches.push(this[i]);
         }
     }
-
+    //new array containing the objects matching the checkbox filter
     return matches;
 }
 // filter ended
 
-createTable()
-jsonData()
-
-// Table creation
+//Dynamic Table creation
 function createTable(value) {
-    jsonData()
+    // jsonData()
     setTimeout(() => {
         if (value == undefined || !value) {
-            value = airportJson.length
-            // value = "10"
+            value = "10"                        //initially show only 10 rows
         }
         else if (value == "all") {
             value = airportJson.length
         }
-        var colHeader = ["name", "icao", "iata", "altitude", "latitude", "longitude", "type"];
-        var header = ["Name", "ICAO", "IATA", "Elev.", "Lat.", "Long.", "Type"]
-        var table = document.createElement("table");
+        var colHeader = ["name", "icao", "iata", "altitude", "latitude", "longitude", "type"]; //header for looping
+        var header = ["Name", "ICAO", "IATA", "Elev.", "Lat.", "Long.", "Type"] //header for showing in UI
+        var table = document.createElement("table"); // creating table tag with other attributes
         table.setAttribute("id", "tableData")
         table.setAttribute("CELLSPACING", "0")
         table.setAttribute("class", "customers-list")
 
-        var tblHead = document.createElement('thead');
+        var tblHead = document.createElement('thead'); //creating thead for
         var rowHead = document.createElement('tr');
         table.appendChild(tblHead);
         tblHead.appendChild(rowHead)
@@ -117,19 +125,21 @@ function createTable(value) {
             rowHead.appendChild(th);
         }
         var tblBody = document.createElement("tbody");
-        for (var i = 0; i < value; i++) {
+        for (var i = 0; i < value; i++) {             //creating row and inserting data 
             var row = document.createElement("tr");
             for (var j = 0; j < colHeader.length; j++) {
                 var cell = document.createElement("td");
-                if (colHeader[j] == 'altitude') {
-                    airportJson[i]['altitude'] = (airportJson[i]['altitude'] * 3.281).toFixed(2) + 'ft'
-                }
-                if (colHeader[j] == 'latitude') {
-                    airportJson[i]['latitude'] = (airportJson[i]['latitude']).toFixed(4)
-                }
-                if (colHeader[j] == 'longitude') {
-                    airportJson[i]['longitude'] = (airportJson[i]['longitude']).toFixed(4)
-                }
+                setTimeout(() => {
+                    if (colHeader[j] == 'altitude') {
+                        airportJson[i]['altitude'] = (airportJson[i]['altitude'] * 3.281).toFixed(2) + 'ft'
+                    }
+                    if (colHeader[j] == 'latitude') {
+                        airportJson[i]['latitude'] = (airportJson[i]['latitude']).toFixed(4)
+                    }
+                    if (colHeader[j] == 'longitude') {
+                        airportJson[i]['longitude'] = (airportJson[i]['longitude']).toFixed(4)
+                    }
+                }, 500);
                 var cellText = document.createTextNode(airportJson[i][colHeader[j]]);
                 cell.appendChild(cellText);
                 row.appendChild(cell);
@@ -140,10 +150,11 @@ function createTable(value) {
         table.appendChild(tblBody);
         var divContainer = document.getElementById("showData");
         divContainer.innerHTML = "";
-        divContainer.appendChild(table);
+        divContainer.appendChild(table);    // appending table to div
     }, 1000);
 }
 
+// Data per page selection
 function valueSelected() {
     setTimeout(() => {
         var e = document.getElementById("selectOption");
@@ -154,6 +165,8 @@ function valueSelected() {
         createTable(strUser)
     }, 500);
 }
+
+// function for checkbox filters on type
 var criteria = [
     {
         Field: "type",
@@ -162,22 +175,43 @@ var criteria = [
 ];
 function filterArr(value) {
     jsonData()
-    setTimeout(() => {
-        if (criteria[0].Values.indexOf(value) == -1) {
-            criteria[0].Values.push(value)
-        }
-        else {
-            var index = criteria[0].Values.indexOf(value)
-            criteria[0].Values.splice(index, 1)
-            if (criteria[0].Values.length == 0) {
-                jsonData()
+    if (criteria[0].Values.length == 0 && value == 'favourite') {
+        setTimeout(() => {
+            if (criteria[0].Values.indexOf(value) == -1) {
+                document.getElementById("tableData").remove()
+                document.getElementById("showData").innerHTML = "No data found"
+                criteria[0].Values.push(value)
             }
-        }
-        airportJson = airportJson.flexFilter(criteria);
-        if (document.getElementById("tableData") != null) {
-            document.getElementById("tableData").remove()
-        }
-        createTable()
-    }, 500);
+            else {
+                document.getElementById("showData").innerHTML = ""
+                var index = criteria[0].Values.indexOf(value)
+                criteria[0].Values.splice(index, 1)
+                if (criteria[0].Values.length == 0) {
+                    jsonData()
+                    createTable()
+                }
+            }
+        }, 1000);
+    }
+    else {
+        setTimeout(() => {
+            if (criteria[0].Values.indexOf(value) == -1) {
+                criteria[0].Values.push(value)
+            }
+            else {
+                var index = criteria[0].Values.indexOf(value)
+                criteria[0].Values.splice(index, 1)
+                if (criteria[0].Values.length == 0) {
+                    jsonData()
+                }
+            }
+            airportJson = airportJson.flexFilter(criteria);
+            if (document.getElementById("tableData") != null) {
+                document.getElementById("tableData").remove()
+            }
+            console.log("criteria[0].Values", criteria[0].Values)
+            createTable()
+        }, 1000);
+    }
 }
 
